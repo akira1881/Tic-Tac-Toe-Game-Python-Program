334cdbed766882648976b9514b352665f73b8ffa
Charina Vallecera <charinavallecera08@gmail.com>
2022-11-19 18:10:52 +0800

    1st commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+
+# Programmed by: Charina C. Vallecera
+# BSCOE 2-2
+# Tic Tac Toe Game
+
+def printBoard(gameValues):
+    print(f" {gameValues[0]} | {gameValues[1]} | {gameValues[2]} ")
+    print(f"---|---|---")
