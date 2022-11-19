c0a48fb617b16058c7faa494350fd5823dc0d161
Charina Vallecera <charinavallecera08@gmail.com>
2022-11-19 18:17:26 +0800

    2nd commit (game values)

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ import os
 def printBoard(gameValues):
     print(f" {gameValues[0]} | {gameValues[1]} | {gameValues[2]} ")
     print(f"---|---|---")
+    print(f" {gameValues[3]} | {gameValues[4]} | {gameValues[5]} ")
+    print(f"---|---|---")
+    print(f" {gameValues[6]} | {gameValues[7]} | {gameValues[8]} ")
